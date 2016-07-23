7b09a6e136f6acfe012f05735ae849c482ed5c67
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2016-07-23 12:45:21 -0300

    aprendendo operadores logicos

---
+++ logica.js
@@ -0,0 +1,33 @@
+//variaveis booleanas
+var t = true
+var f = false
+
+//testando verdadeiros
+
+//igualdade
+t = 1 == 1
+console.log('1 == 1',t)
+t = 1 === 1
+console.log('1 === 1',t)
+//diferença
+t = 1 != 2
+console.log('1 != 2',t)
+t = 1 !== '1'
+console.log("1 !== '1'",t)
+//valores
+t = 2 > 1
+console.log("2 > 1",t)
+t = 1 < 2
+console.log("1 < 2",t)
+
+//operador and
+t = 1 == 1 && 2 == 2
+console.log("1 == 1 && 2 == 2",t)
+t = 1 == 1 & 2 == 2
+console.log("1 == 1 & 2 == 2",t)
+
+//operador or
+t = 1 == 1 || 2 == 3
+console.log("1 == 1 || 2 == 3",t)
+t = 1 == 2 || 2 == 2
+console.log("1 == 2 || 2 == 2",t)
