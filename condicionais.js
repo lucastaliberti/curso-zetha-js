51ceff6c598f536321073617ec040378a32101ce
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2016-07-23 13:29:47 -0300

    aprendendo condicionais

---
+++ condicionais.js
@@ -0,0 +1,17 @@
+var maior = 2
+var menor = 1
+
+//com os blocos
+if(maior > menor) //operador lógico
+{
+  console.log(maior,'>',menor,'estou dentro do if')
+}
+else
+{
+  console.log(maior,'<',menor,'estou dentro do else')
+}
+//sem os blocos
+if(maior < menor)
+  console.log(maior,'>',menor,'estou dentro do if')
+else
+  console.log(maior,'<',menor,'estou dentro do else')
