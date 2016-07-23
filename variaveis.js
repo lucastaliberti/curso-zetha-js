8e4cf535cb76be2a3234be473115505a4698f520
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2016-07-23 12:16:54 -0300

    aprendendo sobre variaveis no javascript

---
+++ variaveis.js
@@ -0,0 +1,25 @@
+var meuInteiro = 1
+var meuFloat = 2.5
+
+//imprimindo numeros
+console.log('meuInteiro: ', meuInteiro)
+console.log('meuFloat: ', meuFloat)
+
+var minhaSoma = meuFloat + meuInteiro
+
+//imprimindo soma
+console.log('minhaSoma: ', minhaSoma)
+
+var minhaString = 'minhaString'
+var minhaOutraString = 'minhaOutraString'
+
+//imprimindo string
+console.log(minhaString)
+
+// concatenando strings
+console.log(minhaString + minhaOutraString)
+
+// concatenando strings + numeros
+console.log(minhaString + meuInteiro + meuFloat)
+console.log( meuInteiro + meuFloat + minhaString)
+console.log(minhaString + (meuInteiro + meuFloat))
