7d252281b6dc41c2b0d1aab3cb6f051cf0401fe0
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2016-07-23 13:50:24 -0300

    aprendendo repeticoes

---
+++ repeticoes.js
@@ -0,0 +1,19 @@
+//repetição while
+i = 1
+
+while(i<=10)
+{
+  console.log('estou dentro do while',i)
+  i++
+}
+
+//repetição for
+for(x = 1;x<=10;x++)
+{
+  console.log('estou dentro do for',x)
+}
+
+for(x = 1;x<=10;x+=2)
+{
+  console.log('estou dentro do for',x)
+}
